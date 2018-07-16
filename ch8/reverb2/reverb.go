8dc9f160c777275e4e2e85bd13925a77e8b29cb4
Brooks.tang <754253197@qq.com>
2018-07-16 16:39:09 +0800

    修改reverb2

--- ch8/reverb2/reverb.go
+++ ch8/reverb2/reverb.go
@@ -13,24 +13,37 @@ import (
 	"net"
 	"strings"
 	"time"
+	"sync"
 )
 
-func echo(c net.Conn, shout string, delay time.Duration) {
-	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
-	time.Sleep(delay)
-	fmt.Fprintln(c, "\t", shout)
-	time.Sleep(delay)
-	fmt.Fprintln(c, "\t", strings.ToLower(shout))
-}
+//func echo(c net.Conn, shout string, delay time.Duration) {
+//	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+//	time.Sleep(delay)
+//	fmt.Fprintln(c, "\t", shout)
+//	time.Sleep(delay)
+//	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+//}
 
 //!+
 func handleConn(c net.Conn) {
 	input := bufio.NewScanner(c)
+	var wg sync.WaitGroup
 	for input.Scan() {
-		go echo(c, input.Text(), 1*time.Second)
+		wg.Add(1)
+		go func(c net.Conn, shout string, delay time.Duration) {
+			defer wg.Done()
+			fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+			time.Sleep(delay)
+			fmt.Fprintln(c, "\t", shout)
+			time.Sleep(delay)
+			fmt.Fprintln(c, "\t", strings.ToLower(shout))
+		}(c, input.Text(), 1*time.Second)
+
 	}
 	// NOTE: ignoring potential errors from input.Err()
-	c.Close()
+	wg.Wait()
+	cw := c.(*net.TCPConn)
+	cw.CloseWrite()
 }
 
 //!-
